933e11a9d51f3336b7fbd49b02a5f87fc3a91f2a
Jenn <huynhjennt@gmail.com>
2018-09-11 15:20:21 -0700

    Set up jest/enzyme and added one working test

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'isomorphic-fetch';
 
 class App extends React.Component {
   constructor(props) {
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          'presets': ['env', 'react'],
+          'presets': ['env', 'react', 'es2015'],
         }
       }
     ]
