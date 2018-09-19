5fa96fb8a9bd111170641915e0944aeb491f30ed
Jenn <huynhjennt@gmail.com>
2018-09-19 14:08:38 -0700

    add cors header

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(
-  <App homeId={window.location.pathname}/>,
-  document.getElementById('app')
-);
+// ReactDOM.render(
+//   <App homeId={window.location.pathname}/>,
+//   document.getElementById('app')
+// );
 
 window.Description = App;
--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const app = express();
 console.log(__dirname);
 app.use("/:homeId", express.static(path.join(__dirname, "./../public/")));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get("/descriptions/:homeId", (req, res) => {
   db.retrieve(req.params.homeId, (err, data) => {
     if (err) {
