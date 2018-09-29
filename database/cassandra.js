e5c02054e6a1f539a3850467590d3a3a1e0402ef
Tinh <tinhkle@Doxie.local>
2018-09-28 18:12:30 -0700

    testing cassandra query with 10M records

--- database/cassandra.js
+++ database/cassandra.js
@@ -9,3 +9,14 @@ client.connect((err) => {
   console.log("cassandra connected");
 });
 
+const query = "select * from houses where homeid = 10000000";
+client.execute(query, (err, result) => {
+  console.time('timer');
+  if (err) {
+  	console.log(err);
+  	return;
+  }
+  console.log(result.rows[0])
+  console.timeEnd('timer')
+});
+
